a976a8c8568fd1f6bb39cf76dcba46b97d2b66ee
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-04-29 01:34:10 +0700

    feat: use the println() method to print numbers

---
+++ src/output/PrintNumber.java
@@ -0,0 +1,10 @@
+package output;
+
+public class PrintNumber {
+
+    public static void main(String[] args) {
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+    }
+}
