e0b296be63e4575c26266e5a7568b5390031eb0f
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-04-29 01:36:03 +0700

    feat: use a multi-line comment

---
+++ src/comment/MultiLine.java
@@ -0,0 +1,12 @@
+package comment;
+
+public class MultiLine {
+
+    public static void main(String[] args) {
+        /*
+        The code below will print the words Hello World!
+        to the screen, and it is amazing
+         */
+        System.out.println("Hello World!");
+    }
+}
