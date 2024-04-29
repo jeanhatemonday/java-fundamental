b350634f73e94610588e603ec6d6febfcc3d90fa
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-04-30 01:41:49 +0700

    feat: non-primitive data types

---
+++ src/datatype/NonPrimitiveData.java
@@ -0,0 +1,13 @@
+package datatype;
+
+public class NonPrimitiveData {
+
+    public static void main(String[] args) {
+        // Non-primitive types are Strings, Arrays, etc.
+        String greeting = "Hello";
+        System.out.println(greeting);
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars[0]);
+    }
+}
