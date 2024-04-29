8f6a52be936768ff9db66e94ae0e75b99ef16b96
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-04-30 01:28:42 +0700

    feat: create a variable and assign the value

---
+++ src/variable/DeclareVariable.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class DeclareVariable {
+
+    public static void main(String[] args) {
+        // Create a variable called name of type String and assign it the value "John":
+        String name = "John";
+        System.out.println(name);
+
+        // Create a variable called myNum of type int and assign it the value 15:
+        int myNum1 = 15;
+        System.out.println(myNum1);
+
+        // Create a variable without assigning the value, and assign the value later:
+        int myNum2;
+        myNum2 = 16;
+        System.out.println(myNum2);
+    }
+}
