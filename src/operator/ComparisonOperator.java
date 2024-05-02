b1c6b5185792d817c9a286bda7c479470b7f6850
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-05-02 07:21:24 +0700

    feat: compare two values or variables

---
+++ src/operator/ComparisonOperator.java
@@ -0,0 +1,25 @@
+package operator;
+
+public class ComparisonOperator {
+
+    public static void main(String[] args) {
+        /*
+        Operator	Name	                    Example
+        ==	        Equal to	                x == y
+        !=	        Not equal	                x != y
+        >	        Greater than	            x > y
+        <	        Less than	                x < y
+        >=	        Greater than or equal to	x >= y
+        <=	        Less than or equal to	    x <= y
+         */
+        int x = 5;
+        int y = 3;
+
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(x > y);
+        System.out.println(x < y);
+        System.out.println(x >= y);
+        System.out.println(x <= y);
+    }
+}
