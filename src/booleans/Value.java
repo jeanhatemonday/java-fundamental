2a52d1316b3e76d51e20e317091703987859460a
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-05-05 19:47:33 +0700

    feat: boolean value

---
+++ src/booleans/Value.java
@@ -0,0 +1,11 @@
+package booleans;
+
+public class Value {
+
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+    }
+}
