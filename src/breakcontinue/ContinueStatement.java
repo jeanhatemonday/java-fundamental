7b81e8ca88ab993fe15f4237899295457d5e4c07
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-05-10 16:21:15 +0700

    feat: continue statement

---
+++ src/breakcontinue/ContinueStatement.java
@@ -0,0 +1,13 @@
+package breakcontinue;
+
+public class ContinueStatement {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
