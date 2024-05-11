73e5e541c6ad74a2e0ddeb9bb2b0766006d3d7d9
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-05-11 07:50:30 +0700

    feat: loop through the array elements

---
+++ src/array/ForLoop.java
@@ -0,0 +1,11 @@
+package array;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+    }
+}
