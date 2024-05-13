5248b6eb21cb2d14a4b4c107d55006c9a15bf014
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-05-13 08:43:31 +0700

    feat: parameter and argument

---
+++ src/parameter/ParameterAndArgument.java
@@ -0,0 +1,20 @@
+package parameter;
+
+public class ParameterAndArgument {
+
+    static void myMethod(String firstName) {
+        System.out.println(firstName + " Refsnes");
+    }
+
+    public static void main(String[] args) {
+        /*
+        When a parameter is passed to the method,
+        it is called an argument.
+        So, from the example above: firstName is a parameter,
+        while Liam, Jenny and Anja are arguments.
+         */
+        myMethod("Liam");
+        myMethod("Jenny");
+        myMethod("Anja");
+    }
+}
