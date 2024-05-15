71ac79f3a7c82f31c4b3256b57c1faf56f95a1de
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-05-15 09:45:48 +0700

    feat: method scope

---
+++ src/scope/Method.java
@@ -0,0 +1,12 @@
+package scope;
+
+public class Method {
+
+    public static void main(String[] args) {
+        // Code here cannot use x
+        int x = 100;
+
+        // Code here can use x
+        System.out.println(x);
+    }
+}
