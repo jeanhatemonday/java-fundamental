1475b0d8c3a2c88b2fb94dec84f5db904013ab09
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-05-17 09:35:34 +0700

    feat: class and object

---
+++ src/oop/ClassObject.java
@@ -0,0 +1,15 @@
+package oop;
+
+public class ClassObject {
+
+    int x = 5;
+
+    public static void main(String[] args) {
+        // Create an object called "myObj" and print the value of x
+        ClassObject myObj1 = new ClassObject();
+        System.out.println(myObj1.x);
+
+        ClassObject myObj2 = new ClassObject();
+        System.out.println(myObj2.x);
+    }
+}
