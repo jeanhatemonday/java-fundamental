2a81fdb61c1deabc611178c148bae76aa2efcd8b
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-05-19 08:54:01 +0700

    feat: class method

---
+++ src/oop/ClassMethod.java
@@ -0,0 +1,20 @@
+package oop;
+
+public class ClassMethod {
+
+    // Create a fullThrottle() method
+    public void fullThrottle() {
+        System.out.println("The car is going as fast as it can!");
+    }
+
+    // Create a speed() method and add a parameter
+    public void speed(int maxSpeed) {
+        System.out.println("Max speed is: " + maxSpeed);
+    }
+
+    public static void main(String[] args) {
+        ClassMethod myCar = new ClassMethod();
+        myCar.fullThrottle();
+        myCar.speed(200);
+    }
+}
