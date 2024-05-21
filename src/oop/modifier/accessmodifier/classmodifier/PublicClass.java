ed70e483042b7baa3960f0687c0ea35ece6132f3
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-05-21 09:24:40 +0700

    feat: public modifier

---
+++ src/oop/modifier/accessmodifier/classmodifier/PublicClass.java
@@ -0,0 +1,12 @@
+package oop.modifier.accessmodifier.classmodifier;
+
+public class PublicClass {
+
+    /*
+    Modifier	Description
+    public	    The class is accessible by any other class
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
