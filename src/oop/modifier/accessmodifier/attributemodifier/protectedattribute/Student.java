c832367ab8ad510c9f5303a706939ae8075f0d57
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-05-22 09:50:56 +0700

    feat: protected modifier

---
+++ src/oop/modifier/accessmodifier/attributemodifier/protectedattribute/Student.java
@@ -0,0 +1,14 @@
+package oop.modifier.accessmodifier.attributemodifier.protectedattribute;
+
+public class Student extends Person {
+
+    private int graduationYear = 2018;
+
+    public static void main(String[] args) {
+        Student myObj = new Student();
+        System.out.println("Name: " + myObj.firstName + " " + myObj.lastName);
+        System.out.println("Email: " + myObj.email);
+        System.out.println("Age: " + myObj.age);
+        System.out.println("Graduation Year: " + myObj.graduationYear);
+    }
+}
