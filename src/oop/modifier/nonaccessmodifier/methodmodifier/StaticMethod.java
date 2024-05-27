21914d2297082e2af22ed17f23c40b8e7a04caf5
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-05-27 08:21:38 +0700

    feat: static modifier

---
+++ src/oop/modifier/nonaccessmodifier/methodmodifier/StaticMethod.java
@@ -0,0 +1,16 @@
+package oop.modifier.nonaccessmodifier.methodmodifier;
+
+public class StaticMethod {
+
+    /*
+    Modifier    Description
+    static      Methods belongs to the class, rather than an object
+     */
+    static void myStaticMethod() {
+        System.out.println("Static methods can be called without creating objects");
+    }
+
+    public static void main(String[] args) {
+        myStaticMethod();
+    }
+}
