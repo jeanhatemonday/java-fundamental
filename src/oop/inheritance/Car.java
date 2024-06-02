bdf2f29e787d75ed0116020921ce5cfc383385b2
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-06-02 15:53:16 +0700

    feat: inheritance

---
+++ src/oop/inheritance/Car.java
@@ -0,0 +1,12 @@
+package oop.inheritance;
+
+public class Car extends Vehicle {
+
+    private String modelName = "Mustang";
+
+    public static void main(String[] args) {
+        Car myFastCar = new Car();
+        myFastCar.honk();
+        System.out.println(myFastCar.brand + " " + myFastCar.modelName);
+    }
+}
