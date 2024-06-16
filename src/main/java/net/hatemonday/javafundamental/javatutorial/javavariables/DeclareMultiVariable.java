deacf842f6613eee3a8a9fe99ee23af05c2765a5
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-06-16 08:33:27 +0700

    feat: declare many variables

---
+++ src/main/java/net/hatemonday/javafundamental/javatutorial/javavariables/DeclareMultiVariable.java
@@ -0,0 +1,13 @@
+package net.hatemonday.javafundamental.javatutorial.javavariables;
+
+public class DeclareMultiVariable {
+
+    public static void main(String[] args) {
+        int x = 5, y = 6, z = 50;
+        System.out.println(x + y + z);
+
+        int a, b, c;
+        a = b = c = 50;
+        System.out.println(a + b + c);
+    }
+}
