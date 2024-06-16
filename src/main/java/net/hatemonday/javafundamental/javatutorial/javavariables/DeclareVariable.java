7bd217a6fdf2dba8e42a9100d8ae6f5baba809fe
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-06-16 08:32:19 +0700

    feat: declaring variables

---
+++ src/main/java/net/hatemonday/javafundamental/javatutorial/javavariables/DeclareVariable.java
@@ -0,0 +1,16 @@
+package net.hatemonday.javafundamental.javatutorial.javavariables;
+
+public class DeclareVariable {
+
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println(name);
+
+        int myNum1 = 15;
+        System.out.println(myNum1);
+
+        int myNum2;
+        myNum2 = 16;
+        System.out.println(myNum2);
+    }
+}
