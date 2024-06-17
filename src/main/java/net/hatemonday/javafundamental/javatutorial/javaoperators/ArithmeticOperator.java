a04b344a50f15d4b2ab6b21b8ab9ab38c024e4fb
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-06-17 09:38:04 +0700

    feat: arithmetic operators

--- src/main/java/net/hatemonday/javafundamental/javatutorial/javaoperators/ArithmeticOperator.java
+++ src/main/java/net/hatemonday/javafundamental/javatutorial/javaoperators/ArithmeticOperator.java
@@ -5,13 +5,13 @@ public class ArithmeticOperator {
     public static void main(String[] args) {
         /*
         Operator	Name	            Description                                 Example
-        +	        Addition	        Adds together two values                    x + y
-        -	        Subtraction	        Subtracts one value from another            x - y
-        *	        Multiplication	    Multiplies two values                       x * y
-        /	        Division	        Divides one value by another                x / y
-        %	        Modulus	            Returns the division remainder              x % y
-        ++	        Increment	        Increases the value of a variable by 1      ++x
-        --	        Decrement	        Decreases the value of a variable by 1      --x
+        +	        Addition            Adds together two values                    x + y
+        -	        Subtraction         Subtracts one value from another            x - y
+        *	        Multiplication      Multiplies two values                       x * y
+        /	        Division            Divides one value by another                x / y
+        %	        Modulus             Returns the division remainder              x % y
+        ++	        Increment           Increases the value of a variable by 1      ++x
+        --	        Decrement           Decreases the value of a variable by 1      --x
          */
         int x = 5;
         int y = 3;
