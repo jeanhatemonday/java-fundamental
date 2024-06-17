bcf6275e32a964b0f2e648aaf4d8e816be450fb5
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-06-17 09:45:46 +0700

    feat: java comparison operators

--- src/main/java/net/hatemonday/javafundamental/javatutorial/javaoperators/ComparisonOperator.java
+++ src/main/java/net/hatemonday/javafundamental/javatutorial/javaoperators/ComparisonOperator.java
@@ -4,13 +4,13 @@ public class ComparisonOperator {
 
     public static void main(String[] args) {
         /*
-        Operator	Name	                    Example
-        ==	        Equal to	                x == y
-        !=	        Not equal	                x != y
-        >	        Greater than	            x > y
-        <	        Less than	                x < y
-        >=	        Greater than or equal to	x >= y
-        <=	        Less than or equal to	    x <= y
+        Operator	Name                        Example
+        ==	        Equal to                    x == y
+        !=	        Not equal                   x != y
+        >	        Greater than                x > y
+        <	        Less than                   x < y
+        >=	        Greater than or equal to    x >= y
+        <=	        Less than or equal to       x <= y
          */
         int x = 5;
         int y = 3;
