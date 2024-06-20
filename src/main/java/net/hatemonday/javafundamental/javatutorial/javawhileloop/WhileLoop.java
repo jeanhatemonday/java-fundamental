a57c59b7b0f365c396201e5728230c4b6d25e0a6
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-06-20 10:15:04 +0700

    feat: java while loop

--- src/main/java/net/hatemonday/javafundamental/javatutorial/javawhileloop/WhileLoop.java
+++ src/main/java/net/hatemonday/javafundamental/javatutorial/javawhileloop/WhileLoop.java
@@ -1,9 +1,10 @@
-package whileloop;
+package net.hatemonday.javafundamental.javatutorial.javawhileloop;
 
 public class WhileLoop {
 
     public static void main(String[] args) {
         int i = 0;
+
         while (i < 5) {
             System.out.println(i);
             i++;
