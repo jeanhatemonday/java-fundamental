54868a8f1bf48e5440d88ee1f4ac2e6b8288fc47
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-06-21 20:11:50 +0700

    feat: for each loop

--- src/main/java/net/hatemonday/javafundamental/javatutorial/javaforloop/ForEachLoop.java
+++ src/main/java/net/hatemonday/javafundamental/javatutorial/javaforloop/ForEachLoop.java
@@ -1,9 +1,10 @@
-package forloop;
+package net.hatemonday.javafundamental.javatutorial.javaforloop;
 
 public class ForEachLoop {
 
     public static void main(String[] args) {
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
         for (String i : cars) {
             System.out.println(i);
         }
