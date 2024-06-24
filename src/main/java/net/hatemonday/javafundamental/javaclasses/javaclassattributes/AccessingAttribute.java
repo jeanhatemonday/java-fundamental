5c84dd9d0f97384aac881ed02f7e25701ff6e8f6
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-06-24 11:10:04 +0700

    feat: accessing attributes

---
+++ src/main/java/net/hatemonday/javafundamental/javaclasses/javaclassattributes/AccessingAttribute.java
@@ -0,0 +1,11 @@
+package net.hatemonday.javafundamental.javaclasses.javaclassattributes;
+
+public class AccessingAttribute {
+
+    int x = 5;
+
+    public static void main(String[] args) {
+        AccessingAttribute myObj = new AccessingAttribute();
+        System.out.println(myObj.x);
+    }
+}
