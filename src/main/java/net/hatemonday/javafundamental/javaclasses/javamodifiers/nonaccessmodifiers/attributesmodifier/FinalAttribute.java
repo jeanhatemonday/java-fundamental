762339089dae1e19d2276739c7b22bbff61b72b0
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-06-29 11:03:44 +0700

    feat: final attribute modifier

--- src/main/java/net/hatemonday/javafundamental/javaclasses/javamodifiers/nonaccessmodifiers/attributesmodifier/FinalAttribute.java
+++ src/main/java/net/hatemonday/javafundamental/javaclasses/javamodifiers/nonaccessmodifiers/attributesmodifier/FinalAttribute.java
@@ -1,18 +1,18 @@
-package net.hatemonday.javafundamental.javaclasses.javamodifiers.nonaccessmodifiers.attributesmodifier;
-
-public class FinalAttribute {
-
-    /*
-    Modifier    Description
-    final       Attributes cannot be overridden/modified
-     */
-    final int x = 10;
-    final double PI = 3.14;
-
-    public static void main(String[] args) {
-        FinalAttribute myObj = new FinalAttribute();
-        myObj.x = 50;
-        myObj.PI = 25;
-        System.out.println(myObj.x);
-    }
-}
+// package net.hatemonday.javafundamental.javaclasses.javamodifiers.nonaccessmodifiers.attributesmodifier;
+//
+// public class FinalAttribute {
+//
+//     /*
+//     Modifier    Description
+//     final       Attributes cannot be overridden/modified
+//      */
+//     final int x = 10;
+//     final double PI = 3.14;
+//
+//     public static void main(String[] args) {
+//         FinalAttribute myObj = new FinalAttribute();
+//         myObj.x = 50;
+//         myObj.PI = 25;
+//         System.out.println(myObj.x);
+//     }
+// }
