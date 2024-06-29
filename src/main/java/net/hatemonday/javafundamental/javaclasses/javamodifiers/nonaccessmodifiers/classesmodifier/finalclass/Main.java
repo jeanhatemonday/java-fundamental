743685aa33ba352117304244867f2b9bcdcd5e84
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-06-29 11:04:12 +0700

    feat: final class modifier

--- src/main/java/net/hatemonday/javafundamental/javaclasses/javamodifiers/nonaccessmodifiers/classesmodifier/finalclass/Main.java
+++ src/main/java/net/hatemonday/javafundamental/javaclasses/javamodifiers/nonaccessmodifiers/classesmodifier/finalclass/Main.java
@@ -1,16 +1,16 @@
-package net.hatemonday.javafundamental.javaclasses.javamodifiers.nonaccessmodifiers.classesmodifier.finalclass;
-
-public class Main extends Vehicle {
-
-    /*
-    Modifier    Description
-    final       The class cannot be inherited by other classes
-     */
-    private String modelName = "Mustang";
-
-    public static void main(String[] args) {
-        Main myFastCar = new Main();
-        myFastCar.honk();
-        System.out.println(myFastCar.brand + " " + myFastCar.modelName);
-    }
-}
+// package net.hatemonday.javafundamental.javaclasses.javamodifiers.nonaccessmodifiers.classesmodifier.finalclass;
+//
+// public class Main extends Vehicle {
+//
+//     /*
+//     Modifier    Description
+//     final       The class cannot be inherited by other classes
+//      */
+//     private String modelName = "Mustang";
+//
+//     public static void main(String[] args) {
+//         Main myFastCar = new Main();
+//         myFastCar.honk();
+//         System.out.println(myFastCar.brand + " " + myFastCar.modelName);
+//     }
+// }
