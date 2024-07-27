dfa2457ef1f559b185e4f3bc09685776a207764f
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-07-27 10:48:05 +0700

    feat: loop through the items of a HashMap with a for-each loop

---
+++ src/main/java/net/hatemonday/javafundamental/javaclasses/javahashmap/LoopThroughAHashMap.java
@@ -0,0 +1,29 @@
+package net.hatemonday.javafundamental.javaclasses.javahashmap;
+
+import java.util.HashMap;
+
+public class LoopThroughAHashMap {
+
+    public static void main(String[] args) {
+        HashMap<String, String> capitalCities = new HashMap<>();
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+
+        // Print keys
+        for (String i : capitalCities.keySet()) {
+            System.out.println(i);
+        }
+
+        // Print values
+        for (String i : capitalCities.values()) {
+            System.out.println(i);
+        }
+
+        // Print keys and values
+        for (String i : capitalCities.keySet()) {
+            System.out.println("key: " + i + " value: " + capitalCities.get(i));
+        }
+    }
+}
