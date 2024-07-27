853cf1a438a8eb411b87e7685b615d80b1b5f734
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-07-28 00:16:14 +0700

    feat: add items in the hashset

---
+++ src/main/java/net/hatemonday/javafundamental/javaclasses/javahashset/AddItem.java
@@ -0,0 +1,16 @@
+package net.hatemonday.javafundamental.javaclasses.javahashset;
+
+import java.util.HashSet;
+
+public class AddItem {
+
+    public static void main(String[] args) {
+        HashSet<String> cars = new HashSet<>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("BMW");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+}
